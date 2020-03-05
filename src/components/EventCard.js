925b751c6d5bc455563b83dfed33e2ddd9411abb
Steven Wang <stevenwang2060@gmail.com>
2020-03-05 15:46:54 -0800

    Styling buttons again but with colors consistent with the landing page

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, CardTitle, CardBody, CardSubtitle } from "reactstrap";
+import { Card, CardTitle, CardBody, CardSubtitle, Button } from "reactstrap";
 
 // display event info
 const EventCard = props => {
@@ -11,7 +11,9 @@ const EventCard = props => {
       <CardBody>
           <CardTitle>Event Name: {props.event.potluckName }</CardTitle>
           <CardSubtitle>Date: {props.event.date}</CardSubtitle>
+          <Button className= "second-button">
           <Link to={`/event/${props.event.id}`}>View Details</Link>
+          </Button>
       </CardBody>
     </Card>
   );
